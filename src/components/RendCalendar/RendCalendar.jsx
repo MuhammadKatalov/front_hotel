dcc3239d7cccd830f8c519a98bec7b613abebe01
Shamil <shamil>
2022-06-29 14:20:59 +0300

    делаю аренду

--- src/components/RendCalendar/RendCalendar.jsx
+++ src/components/RendCalendar/RendCalendar.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+import { postRends } from "../../features/RendSlice";
+import { useSelector, useDispatch } from "react-redux/es/exports";
 
 const RendCalendar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const dispatch = useDispatch();
+  const rends = useSelector(state => state.rends)
+console.log(rends);
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [checkIn, checkOut] = dateRange;
+
+  const checkInDate = new Date(checkIn);
+
+  const checkInMonth = checkInDate.getUTCMonth() + 1;
+  const checkInDay = checkInDate.getUTCDate();
+  const checkInYear = checkInDate.getUTCFullYear();
+
+  const checkOutDate = new Date(checkOut);
+
+  const checkOutMonth = checkOutDate.getUTCMonth() + 1;
+  const checkOutDay = checkOutDate.getUTCMonth();
+  const checkOutYear = checkOutDate.getUTCFullYear();
+  
+  const checkInValue = checkInYear + "-" + checkInMonth + "-" + checkInDay;
+  const checkOutValue = checkOutYear + "-" + checkOutMonth + "-" + checkOutDay;
+
+  const handleRend = () => {
+    dispatch(postRends({ registrationDate: checkInValue, releaseDate: checkOutValue }))
+  }
+
+  return (
+    <div className="calendar">
+      <DatePicker
+        selectsRange={true}
+        startDate={checkIn}
+        endDate={checkOut}
+        onChange={(update) => {
+          setDateRange(update);
+        }}
+        isClearable={false}
+      />
+      <button onClick={() => handleRend(checkInDate, checkOutDate)}>арендовать</button>
+    </div>
+  );
 };
 
 export default RendCalendar;
--- src/features/RendSlice.js
+++ src/features/RendSlice.js
@@ -5,12 +5,39 @@ const initialState = {
     rends: []
 }
 
+export const postRends = createAsyncThunk(
+    'rends/fetchRands',
+    async ({registrationDate, releaseDate, service, id}, thunkAPI) => {
+        try {
+            const state = thunkAPI.getState();
+            const res = await fetch(`/rends/${id}`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${state.auth.token}`,
+                  },
+                body: JSON.stringify({
+                    registrationDate,
+                    releaseDate,
+                    service
+                })
+            });
+            const data = await res.json();
+            return data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e)
+        }
+    }
+)
+
 export const rendsSlice = createSlice({
     name: 'rends',
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        
+        builder
+        .addCase(postRends.fulfilled, (state, action) => {
+            state.rends.push(action.payload)
+        })
     }
 })
 
