df6660949088b0003528eeb5e14647c19ea05c11
zelim <kveyk777@mail.ru>
2022-07-01 17:24:08 +0300

    исправил ошикбку

--- src/components/pages/Room/index.jsx
+++ src/components/pages/Room/index.jsx
@@ -5,27 +5,26 @@ import { fetchRoom } from "../../../features/RoomSlice";
 
 const Room = () => {
   const room = useSelector((state) => state.room.room);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchRoom())
   }, [dispatch]);
 
-  if (!room.length) {
+  if (!room) {
     return "...загрузка";
   }
 
   return (
     <>
-      <div>описание что на улице --- {room[0].outside}</div>
-      <div>описание что внутри --- {room[0].inside}</div>
-      <div>цена аренды на сутки --- {room[0].price}</div>
-      <div>количество комнат в хижине --- {room[0].roomsCounter}</div>
-      <div>максимум человек --- {room[0].numberPerson}</div>
+      <div>описание что на улице --- {room.outside}</div>
+      <div>описание что внутри --- {room.inside}</div>
+      <div>цена аренды на сутки --- {room.price}</div>
+      <div>количество комнат в хижине --- {room.roomsCounter}</div>
+      <div>максимум человек --- {room.numberPerson}</div>
       <div>
-        <img src={room[0].images[0]} />
-        <img src={room[0].images[1]} />
+        <img src={room.images[0]} />
+        <img src={room.images[1]} />
       </div>
       <button>Арендовать</button>
     </>
