7abedbce9268b056febb545c1d3c8869ff46570d
Muhammad <magomed.katalov@bk.ru>
2022-07-02 14:42:37 +0300

    пофиксил баг c undefined

--- src/components/pages/WelcomePage/Welcome.jsx
+++ src/components/pages/WelcomePage/Welcome.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MapComponent from './Map';
 
 const Welcome = () => {
     return (
         <div>
-            
+            <MapComponent />
         </div>
     );
 };
